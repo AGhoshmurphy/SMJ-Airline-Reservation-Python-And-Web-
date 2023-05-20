4a6c250ea409eb7a62f9cd8e8c7e6bdc22d0bb97
unknown <mdrjoshep@gmail.com>
2023-05-20 19:47:14 +0530

    first commit

--- main.py
+++ main.py
@@ -1,87 +1,161 @@
-from flask import Flask, render_template, request, redirect
 import mysql.connector
-
-app = Flask(__name__)
+from tkinter import *
+from tkinter.ttk import Combobox
+from tkinter import messagebox
 
 # Connect to MySQL database
-db = mysql.connector.connect(
+conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="",
+    password="sunshine",
     database="smj_airline_reservation"
 )
+cursor = conn.cursor()
 
-cursor = db.cursor()
+# Function to create a new reservation
+def create_reservation():
+    trip_type = trip_var.get()
+    departure_date = departure_var.get()
+    return_date = return_var.get()
+    num_travelers = num_travelers_var.get()
+    total_amount = 200.00 if trip_type == "one_way" else 300.00
+    first_name = first_name_entry.get()
+    last_name = last_name_entry.get()
+    date_of_birth = dob_entry.get()
+    payment_card_number = card_entry.get()
+    reservation_status = "success"
+    try:
+        sql = "INSERT INTO reservations (trip_type, departure_date, return_date, num_travelers, total_amount, first_name, last_name, date_of_birth, payment_card_number, reservation_status) " \
+              "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (trip_type, departure_date, return_date, num_travelers, total_amount, first_name, last_name, date_of_birth, payment_card_number, reservation_status)
+        cursor.execute(sql, values)
+        conn.commit()
+        messagebox.showinfo("Success", "Reservation created successfully!")
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
 
-# Create table if not exists
-cursor.execute("""CREATE TABLE IF NOT EXISTS reservations (
-                  id INT AUTO_INCREMENT PRIMARY KEY,
-                  trip_type ENUM('one_way', 'return') NOT NULL,
-                  departure_date DATE NOT NULL,
-                  return_date DATE,
-                  num_travelers INT NOT NULL,
-                  total_amount DECIMAL(10, 2) NOT NULL,
-                  first_name VARCHAR(50),
-                  last_name VARCHAR(50),
-                  dob DATE,
-                  payment_info VARCHAR(100)
-                 )""")
+# Function to read reservations
+def read_reservations():
+    cursor.execute("SELECT * FROM reservations")
+    result = cursor.fetchall()
+    return result
 
-# Commit changes and close cursor
-db.commit()
-cursor.close()
-db.close()
+# Function to delete a reservation
+def delete_reservation():
+    reservation_id = delete_id_var.get()
+    try:
+        sql = "DELETE FROM reservations WHERE id = %s"
+        values = (reservation_id,)
+        cursor.execute(sql, values)
+        conn.commit()
+        messagebox.showinfo("Success", "Reservation deleted successfully!")
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
 
-# Route for homepage
-@app.route('/')
-def home():
-    return render_template('index.html')
+# Function to update a reservation
+def update_reservation():
+    reservation_id = update_id_var.get()
+    trip_type = trip_var.get()
+    departure_date = departure_var.get()
+    return_date = return_var.get()
+    num_travelers = num_travelers_var.get()
+    total_amount = 200.00 if trip_type == "one_way" else 300.00
+    first_name = first_name_entry.get()
+    last_name = last_name_entry.get()
+    date_of_birth = dob_entry.get()
+    payment_card_number = card_entry.get()
+    reservation_status = "success"
+    try:
+        sql = "UPDATE reservations SET trip_type = %s, departure_date = %s, return_date = %s, num_travelers = %s, total_amount = %s, first_name = %s, last_name = %s, date_of_birth = %s, payment_card_number = %s, reservation_status = %s WHERE id = %s"
+        values = (trip_type, departure_date, return_date, num_travelers, total_amount, first_name, last_name, date_of_birth, payment_card_number, reservation_status, reservation_id)
+        cursor.execute(sql, values)
+        conn.commit()
+        messagebox.showinfo("Success", "Reservation updated successfully!")
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
 
-# Route for reservation form
-@app.route('/reservation', methods=['GET', 'POST'])
-def reservation():
-    if request.method == 'POST':
-        # Get form data
-        trip_type = request.form['trip_type']
-        departure_date = request.form['departure_date']
-        return_date = request.form['return_date']
-        num_travelers = int(request.form['num_travelers'])
+# Function to display reservations in a table
+def display_reservations():
+    result = read_reservations()
+    if result:
+        display_text.delete("1.0", END)
+        display_text.insert(END, "ID\tTrip Type\tDeparture Date\tReturn Date\tNum Travelers\tTotal Amount\tFirst Name\tLast Name\tDate of Birth\tPayment Card Number\tReservation Status\n")
+        for row in result:
+            display_text.insert(END, f"{row[0]}\t{row[1]}\t{row[2]}\t{row[3]}\t{row[4]}\t{row[5]}\t{row[6]}\t{row[7]}\t{row[8]}\t{row[9]}\t{row[10]}\n")
+    else:
+        display_text.delete("1.0", END)
+        display_text.insert(END, "No reservations found.")
 
-        # Calculate total amount
-        total_amount = 100.0 * num_travelers  # Example calculation, you can change as needed
+# Create GUI window
+root = Tk()
+root.title("Airline Reservation System")
 
-        # Get traveler details
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        dob = request.form['dob']
-        payment_info = request.form['payment_info']
+# Create labels
+trip_label = Label(root, text="Trip Type:")
+departure_label = Label(root, text="Departure Date:")
+return_label = Label(root, text="Return Date:")
+num_travelers_label = Label(root, text="Number of Travelers:")
+first_name_label = Label(root, text="First Name:")
+last_name_label = Label(root, text="Last Name:")
+dob_label = Label(root, text="Date of Birth:")
+card_label = Label(root, text="Payment Card Number:")
+delete_id_label = Label(root, text="Reservation ID for Deletion:")
+update_id_label = Label(root, text="Reservation ID for Update:")
 
-        # Insert reservation into database
-        db = mysql.connector.connect(
-            host="localhost",
-            user="username",
-            password="password",
-            database="smj_airline_reservation"
-        )
-        cursor = db.cursor()
-        cursor.execute("""INSERT INTO reservations 
-                          (trip_type, departure_date, return_date, num_travelers, total_amount,
-                           first_name, last_name, dob, payment_info)
-                          VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""",
-                       (trip_type, departure_date, return_date, num_travelers, total_amount,
-                        first_name, last_name, dob, payment_info))
-        db.commit()
-        cursor.close()
-        db.close()
+# Create entry fields
+trip_var = StringVar()
+trip_combobox = Combobox(root, textvariable=trip_var, values=["one_way", "round_trip"])
+departure_var = StringVar()
+departure_entry = Entry(root, textvariable=departure_var)
+return_var = StringVar()
+return_entry = Entry(root, textvariable=return_var)
+num_travelers_var = IntVar()
+num_travelers_entry = Entry(root, textvariable=num_travelers_var)
+first_name_entry = Entry(root)
+last_name_entry = Entry(root)
+dob_entry = Entry(root)
+card_entry = Entry(root)
+delete_id_var = IntVar()
+delete_id_entry = Entry(root, textvariable=delete_id_var)
+update_id_var = IntVar()
+update_id_entry = Entry(root, textvariable=update_id_var)
 
-        return redirect('/success')  # Redirect to success page after reservation
+# Create buttons
+create_button = Button(root, text="Create Reservation", command=create_reservation)
+read_button = Button(root, text="Display Reservations", command=display_reservations)
+delete_button = Button(root, text="Delete Reservation", command=delete_reservation)
+update_button = Button(root, text="Update Reservation", command=update_reservation)
 
-    return render_template('reservation.html')
+# Create text box to display reservations
+display_text = Text(root, height=10, width=100)
 
-# Route for success page
-@app.route('/success')
-def success():
-    return render_template('success.html')
+# Grid layout for GUI elements
+trip_label.grid(row=0, column=0, sticky=W)
+trip_combobox.grid(row=0, column=1)
+departure_label.grid(row=1, column=0, sticky=W)
+departure_entry.grid(row=1, column=1)
+return_label.grid(row=2, column=0, sticky=W)
+return_entry.grid(row=2, column=1)
+num_travelers_label.grid(row=3, column=0, sticky=W)
+num_travelers_entry.grid(row=3, column=1)
+first_name_label.grid(row=4, column=0, sticky=W)
+first_name_entry.grid(row=4, column=1)
+last_name_label.grid(row=5, column=0, sticky=W)
+last_name_entry.grid(row=5, column=1)
+dob_label.grid(row=6, column=0, sticky=W)
+dob_entry.grid(row=6, column=1)
+card_label.grid(row=7, column=0, sticky=W)
+card_entry.grid(row=7, column=1)
+create_button.grid(row=8, column=0, columnspan=2)
+delete_id_label.grid(row=9, column=0, sticky=W)
+delete_id_entry.grid(row=9, column=1)
+delete_button.grid(row=10, column=0, columnspan=2)
+update_id_label.grid(row=11, column=0, sticky=W)
+update_id_entry.grid(row=11, column=1)
+update_button.grid(row=12, column=0, columnspan=2)
+read_button.grid(row=13, column=0, columnspan=2)
+display_text.grid(row=14, column=0, columnspan=2)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+root.mainloop()
+
---
+++ smj_airline_reservation_reservations.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: smj_airline_reservation
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `trip_type` enum('one_way','return') DEFAULT NULL,
+  `departure_date` date DEFAULT NULL,
+  `return_date` date DEFAULT NULL,
+  `num_travelers` int DEFAULT NULL,
+  `total_amount` decimal(10,2) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `payment_card_number` varchar(16) DEFAULT NULL,
+  `reservation_status` enum('success','pending','failed') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservations`
+--
+
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+INSERT INTO `reservations` VALUES (1,'one_way','2023-04-10',NULL,2,200.00,'John','Doe','1990-01-01','1234567890123456','success'),(4,'one_way','2023-04-10',NULL,2,200.00,'John','Doe','1990-01-01','1234567890123456','success'),(5,'one_way','2023-04-10',NULL,2,200.00,'John','Doe','1990-01-01','1234567890123456','success'),(6,'one_way','2023-04-10',NULL,2,200.00,'John','Doe','1990-01-01','1234567890123456','success'),(7,'one_way','2023-04-23','2023-04-29',2,200.00,'Alan','Walker','1998-04-11','1234567890123456','success'),(8,'one_way','2023-04-23','2023-04-29',2,200.00,'Alan','Walker','1998-04-11','1234567890123456','success'),(9,'one_way','2023-04-10',NULL,2,200.00,'John','Doe','1990-01-01','1234567890123456','success');
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-08 19:57:12
